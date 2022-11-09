61509e226c5f3ef36b8238994e7147f9e22ef31b
hieuhn <hieungochoangit@gmail.com>
2022-11-09 23:11:57 +0700

    Added base loading component

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { CSpinner } from "@coreui/react";
+
+const Loading = () => {
+    return <CSpinner component="span" size="sm" variant="grow" aria-hidden="true" />;
+};
+
+export default Loading;
