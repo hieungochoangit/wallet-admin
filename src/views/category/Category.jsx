8854f1957ee25d4dac77eb48302a635381759d22
hieuhn <hieungochoangit@gmail.com>
2022-11-10 11:14:05 +0700

    Added UI table cateogy

--- src/views/category/Category.jsx
+++ src/views/category/Category.jsx
@@ -1,7 +1,35 @@
+import { CButton } from "@coreui/react";
 import React from "react";
+import CategoryTable from "./categoryTable/CategoryTable";
+
+const dataCategory = [
+    {
+        id: 1,
+        categoryName: "Ví da",
+        categorySlug: "vi-da",
+        categoryDesc: "Cac loai vi da vip",
+        categoryType: "VIP",
+        activity: "10 sec ago",
+    },
+    {
+        id: 2,
+        categoryName: "Ví da",
+        categorySlug: "vi-da",
+        categoryDesc: "Cac loai vi da vip",
+        categoryType: "VIP",
+        activity: "10 sec ago",
+    },
+];
 
 const Category = () => {
-    return <div>Category</div>;
+    return (
+        <div>
+            <div className="d-flex justify-content-end">
+                <CButton className="mb-4">Thêm danh mục</CButton>
+            </div>
+            <CategoryTable data={dataCategory} />
+        </div>
+    );
 };
 
 export default Category;
