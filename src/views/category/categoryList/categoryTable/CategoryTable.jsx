1d12034307e367fdf1f5bdc5e1f64995141783b6
hieuhn <hieungochoangit@gmail.com>
2022-11-13 20:29:27 +0700

    Added button action

--- src/views/category/categoryList/categoryTable/CategoryTable.jsx
+++ src/views/category/categoryList/categoryTable/CategoryTable.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CTable, CTableHead, CTableRow, CTableHeaderCell, CTableBody, CTableDataCell, CButton } from "@coreui/react";
+import { CTable, CTableHead, CTableRow, CTableHeaderCell, CTableBody, CButton } from "@coreui/react";
 import { Link } from "react-router-dom";
 
 const CategoryTable = (props) => {
@@ -8,13 +8,49 @@ const CategoryTable = (props) => {
     return (
         <>
             {data.length <= 0 ? (
-                <div>
-                    Chưa có danh mục con, bạn có muốn <Link to="/category/create">thêm</Link> không?
+                <div className="d-flex justify-content-between align-items-center">
+                    <span>
+                        Chưa có danh mục con, bạn có muốn <Link to="/category/create">thêm</Link> không?
+                    </span>
+                    <div className="d-flex gap-2">
+                        <Link>
+                            <CButton color="primary" size="sm">
+                                Xem
+                            </CButton>
+                        </Link>
+                        <Link>
+                            <CButton color="info" size="sm">
+                                Sửa
+                            </CButton>
+                        </Link>
+                        <Link>
+                            <CButton color="dark" size="sm">
+                                Xoá
+                            </CButton>
+                        </Link>
+                    </div>
                 </div>
             ) : (
                 <>
-                    <div className="mb-3">
+                    <div className="mb-3 d-flex justify-content-between align-items-center">
                         <Link to="/category/create">Thêm danh mục con</Link>
+                        <div className="d-flex gap-2">
+                            <Link>
+                                <CButton color="primary" size="sm">
+                                    Xem
+                                </CButton>
+                            </Link>
+                            <Link>
+                                <CButton color="info" size="sm">
+                                    Sửa
+                                </CButton>
+                            </Link>
+                            <Link>
+                                <CButton color="dark" size="sm">
+                                    Xoá
+                                </CButton>
+                            </Link>
+                        </div>
                     </div>
                     <CTable hover bordered>
                         <CTableHead>
